f86e61efd1e57416b2de9d38cb25926715562635
xuhongzhuo <2395958229@qq.com>
2022-02-27 21:42:55 +0800

    fix[dht11]: 修复未导出的bug

--- src/main/java/com/xhz/iotstarter/driver/Dht11Driver.java
+++ src/main/java/com/xhz/iotstarter/driver/Dht11Driver.java
@@ -32,11 +32,6 @@ public class Dht11Driver extends BaseDriver {
      */
     private synchronized float[] getTemperatureAndHumidity() {
         int pin = getPin();
-        if (!isExec()) {
-            // 设备没准备好呢
-            log.info("数据还没准备好呢");
-            return new float[]{255};
-        }
         int laststate = Gpio.HIGH;
         int j = 0;
         dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
@@ -89,6 +84,82 @@ public class Dht11Driver extends BaseDriver {
                 c = -c;
             }
             final float f = c * 1.8f + 32;
+            float[] result = new float[2];
+            result[0] = h;
+            result[1] = c;
+            return result;
+        } else {
+            float[] result = new float[2];
+            result[0] = -1;
+            result[1] = -1;
+            return new float[2];
+        }
+
+    }
+
+    private boolean checkParity() {
+        return dht11_dat[4] == (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3] & 0xFF);
+    }
+
+    /*private synchronized float[] getTemperatureAndHumidity() {
+        int pin = getPin();
+        if (!isExec()) {
+            // 设备没准备好呢
+            log.info("数据还没准备好呢");
+            return new float[]{255};
+        }
+        int laststate = Gpio.HIGH;
+        int j = 0;
+        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+
+        Gpio.pinMode(pin, Gpio.OUTPUT);
+        Gpio.digitalWrite(pin, Gpio.LOW);
+        Gpio.delay(18);
+
+        Gpio.digitalWrite(pin, Gpio.HIGH);
+        Gpio.pinMode(pin, Gpio.INPUT);
+
+        for (int i = 0; i < MAXTIMINGS; i++) {
+            int counter = 0;
+            while (Gpio.digitalRead(pin) == laststate) {
+                counter++;
+                Gpio.delayMicroseconds(1);
+                if (counter == 255) {
+                    break;
+                }
+            }
+
+            laststate = Gpio.digitalRead(pin);
+
+            if (counter == 255) {
+                break;
+            }
+
+            *//* ignore first 3 transitions *//*
+            if (i >= 4 && i % 2 == 0) {
+                *//* shove each bit into the storage bytes *//*
+                dht11_dat[j / 8] <<= 1;
+                if (counter > 16) {
+                    dht11_dat[j / 8] |= 1;
+                }
+                j++;
+            }
+        }
+        // check we read 40 bits (8bit x 5 ) + verify checksum in the last
+        // byte
+        if (j >= 40 && checkParity()) {
+            float h = (float) ((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
+            if (h > 100) {
+                h = dht11_dat[0]; // for DHT11
+            }
+            float c = (float) (((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
+            if (c > 125) {
+                c = dht11_dat[2]; // for DHT11
+            }
+            if ((dht11_dat[2] & 0x80) != 0) {
+                c = -c;
+            }
+            final float f = c * 1.8f + 32;
             float[] res = new float[2];
             res[0] = h; // 湿度
             res[1] = c; // 温度
@@ -98,7 +169,7 @@ public class Dht11Driver extends BaseDriver {
             return new float[]{255};
         }
 
-    }
+    }*/
 
     /**
      * 只获取温度
@@ -149,9 +220,6 @@ public class Dht11Driver extends BaseDriver {
     }
 
 
-    private boolean checkParity() {
-        return dht11_dat[4] == (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3] & 0xFF);
-    }
 
 
     @Override
